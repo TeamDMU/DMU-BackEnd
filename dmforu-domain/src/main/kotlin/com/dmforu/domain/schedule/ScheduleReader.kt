75553a6417d5b4cb990fedd597589a5f67bc8e06
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-09-29 00:17:25 +0900

    feat: Schedule write, read 비지니스 로직 작성

--- dmforu-domain/src/main/kotlin/com/dmforu/domain/schedule/ScheduleReader.kt
+++ dmforu-domain/src/main/kotlin/com/dmforu/domain/schedule/ScheduleReader.kt
@@ -1,26 +1,18 @@
 package com.dmforu.domain.schedule
 
-import com.dmforu.domain.schedule.legacy.LegacySchedule
-import com.dmforu.domain.schedule.legacy.MonthSchedule
 import com.dmforu.domain.schedule.legacy.YearSchedule
 import org.springframework.stereotype.Component
 
 @Component
-class ScheduleReader {
+class ScheduleReader(
+    private val scheduleRepository: ScheduleRepository
+) {
     @Deprecated("Use read() instead.", ReplaceWith("read()"))
     fun readOld(): List<YearSchedule> {
-        val legacySchedule = LegacySchedule(arrayOf("01.01(일)", "01.01(일)"), "신정")
-        val monthSchedule = MonthSchedule(1, listOf(legacySchedule))
-        val yearSchedule = YearSchedule(2024, listOf(monthSchedule))
-
-        return listOf(yearSchedule)
+        return scheduleRepository.readOld().orEmpty()
     }
 
     fun read(): List<Schedule.Year> {
-        val schedule = Schedule(arrayOf("01.01(일)", "01.01(일)"), "신정")
-        val monthSchedule = Schedule.Month(1, listOf(schedule))
-        val yearSchedule = Schedule.Year(2024, listOf(monthSchedule))
-
-        return listOf(yearSchedule)
+        return scheduleRepository.read().orEmpty()
     }
 }
--- dmforu-domain/src/main/kotlin/com/dmforu/domain/schedule/ScheduleService.kt
+++ dmforu-domain/src/main/kotlin/com/dmforu/domain/schedule/ScheduleService.kt
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service
 
 @Service
 class ScheduleService(
-    private val scheduleReader: ScheduleReader
+    private val scheduleReader: ScheduleReader,
+    private val scheduleWriter: ScheduleWriter
 ) {
     @Deprecated("Use read() instead.", ReplaceWith("read()"))
     fun readOld(): List<YearSchedule> {
@@ -14,5 +15,14 @@ class ScheduleService(
 
     fun read(): List<Schedule.Year> {
         return scheduleReader.read()
+    }
+
+    @Deprecated("Use write() instead.", ReplaceWith("write()"))
+    fun writeOld(schedules: List<YearSchedule>) {
+        return scheduleWriter.writeOld(schedules)
+    }
+
+    fun write(schedules: List<Schedule.Year>) {
+        return scheduleWriter.write(schedules)
     }
 }
