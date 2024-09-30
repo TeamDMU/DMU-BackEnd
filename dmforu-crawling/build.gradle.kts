87f369c5ba0c81d88e436544e87d6f4949d8e44e
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-01 03:34:48 +0900

    fix: crawling에 domain 프로젝트 의존성이 빠져있던 부분 수정

--- dmforu-crawling/build.gradle.kts
+++ dmforu-crawling/build.gradle.kts
@@ -1,6 +1,7 @@
 val jsoupVersion: String by project
 
 dependencies {
+    implementation(project(":dmforu-domain"))
     implementation("org.jsoup:jsoup:${jsoupVersion}")
 
     compileOnly("org.springframework:spring-context")
