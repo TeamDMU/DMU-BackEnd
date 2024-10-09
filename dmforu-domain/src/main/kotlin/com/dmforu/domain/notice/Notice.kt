d8b3018b81962c03feb86ff4ef76c5d9ac312173
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-09 22:38:19 +0900

    feat: Notice data class 생성

---
+++ dmforu-domain/src/main/kotlin/com/dmforu/domain/notice/Notice.kt
@@ -0,0 +1,23 @@
+package com.dmforu.domain.notice
+
+import java.time.LocalDate
+
+data class Notice(
+    // 공지사항 번호
+    val number: Int,
+
+    // 공지사항 종류
+    val type: String,
+
+    // 공지사항 날짜
+    val date: LocalDate,
+
+    // 공지사항 제목
+    val title: String,
+
+    // 공지사항 작성자
+    val author: String,
+
+    // 공지사항 URL
+    val url: String
+)
