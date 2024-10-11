1acacd22fad2c5fea9bdc71d706082966abbdeba
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-11 13:52:51 +0900

    feat: SubscribeEntity 필드를 상태변화 메서드 추가

--- dmforu-storage/db-mysql/src/main/kotlin/com/dmforu/storage/db/mysql/subscribe/SubscribeEntity.kt
+++ dmforu-storage/db-mysql/src/main/kotlin/com/dmforu/storage/db/mysql/subscribe/SubscribeEntity.kt
@@ -2,10 +2,7 @@ package com.dmforu.storage.db.mysql.subscribe
 
 import com.dmforu.domain.subscribe.Subscribe
 import com.dmforu.storage.db.mysql.converter.StringListConverter
-import jakarta.persistence.Convert
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 
 @Table(name = "token")
@@ -14,14 +11,18 @@ internal class SubscribeEntity(
     @Id
     val token: String,
 
-    val department: String,
+    @Column(nullable = true)
+    var department: String,
 
     @Convert(converter = StringListConverter::class)
-    val keywords: List<String>?,
+    @Column(name = "keywords_list",nullable = true)
+    var keywords: List<String>,
 
-    val isDepartmentSubscribed: Boolean,
+    @Column(name = "department_onoff")
+    var isDepartmentSubscribed: Boolean,
 
-    val areKeywordsSubscribed: Boolean,
+    @Column(name = "keyword_onoff")
+    var areKeywordsSubscribed: Boolean,
 ) {
     constructor() : this("", "", emptyList(), false, false)
 
@@ -35,5 +36,39 @@ internal class SubscribeEntity(
                 areKeywordsSubscribed = false
             )
         }
+    }
+
+    fun toSubscribe(): Subscribe {
+        return Subscribe(
+            token = token,
+            department = department,
+            keywords = keywords,
+            isDepartmentSubscribed = isDepartmentSubscribed,
+            areKeywordsSubscribed = areKeywordsSubscribed
+        )
+    }
+
+    fun changeDepartment(department: String) {
+        this.department = department
+    }
+
+    fun subscribeDepartment() {
+        this.isDepartmentSubscribed = true
+    }
+
+    fun unsubscribeDepartment() {
+        this.isDepartmentSubscribed = false
+    }
+
+    fun changeKeywords(keywords: List<String>) {
+        this.keywords = keywords
+    }
+
+    fun subscribeKeywords() {
+        this.areKeywordsSubscribed = true
+    }
+
+    fun unsubscribeKeywords() {
+        this.areKeywordsSubscribed = false
     }
 }
