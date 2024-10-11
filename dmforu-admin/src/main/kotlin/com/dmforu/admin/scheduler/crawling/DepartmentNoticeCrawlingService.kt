f7ddfa6fc97101c5826348afcda35bf86137153d
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-11 17:11:24 +0900

    style: NoticeCrawlWirter -> NoticeCrawlServie로 클래스 이름 변경

--- dmforu-admin/src/main/kotlin/com/dmforu/admin/scheduler/crawling/DepartmentNoticeCrawlingService.kt
+++ dmforu-admin/src/main/kotlin/com/dmforu/admin/scheduler/crawling/DepartmentNoticeCrawlingService.kt
@@ -3,15 +3,14 @@ package com.dmforu.admin.scheduler.crawling
 import com.dmforu.crawling.DepartmentNoticeParser
 import com.dmforu.domain.notice.Notice
 import com.dmforu.domain.notice.Major
-import com.dmforu.domain.notice.NoticeCrawlWriter
+import com.dmforu.domain.notice.NoticeCrawlService
 import org.springframework.beans.factory.ObjectProvider
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class DepartmentNoticeCrawlingService(
     private val prototypeBeanProvider: ObjectProvider<DepartmentNoticeParser>,
-    private val noticeCrawlWriter: NoticeCrawlWriter
+    private val noticeCrawlService: NoticeCrawlService
 ) {
     /**
      * Major 열거형의 모든 값을 반복하여 모든 학과의 공지사항을 크롤링한다. <br></br>
@@ -37,7 +36,7 @@ class DepartmentNoticeCrawlingService(
 
         parser.initialize(major)
 
-        val maxNumber: Int? = noticeCrawlWriter.findMaxNumberByType(major.type)
+        val maxNumber: Int? = noticeCrawlService.findMaxNumberByType(major.type)
         val currentMaxNumber = maxNumber ?: 0
 
         while (true) {
@@ -66,7 +65,7 @@ class DepartmentNoticeCrawlingService(
                 return false
             }
 
-            noticeCrawlWriter.write(notice)
+            noticeCrawlService.write(notice)
             // TODO: FCM 메세지 전송을 위한 이벤트 트리거
 //            eventPublisher!!.publishEvent(notice)
             if (notice.isLastInType()) {
