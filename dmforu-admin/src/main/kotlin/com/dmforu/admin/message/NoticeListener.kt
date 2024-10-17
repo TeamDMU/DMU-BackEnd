cdf39442244be73a189103277ed9ffa4736efe26
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-18 03:08:10 +0900

    feat: 공지사항이 추가될 때 알림을 전송하도록 이벤트 리스너 작성

---
+++ dmforu-admin/src/main/kotlin/com/dmforu/admin/message/NoticeListener.kt
@@ -0,0 +1,16 @@
+package com.dmforu.admin.message
+
+import com.dmforu.domain.notice.Notice
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+class NoticeListener (
+    private val messageService: MessageService
+) {
+    @EventListener
+    fun onNoticeMessageSendEventHandler(notice: Notice) {
+        println(notice.title)
+        messageService.sendNoticeMessage(notice)
+    }
+}
