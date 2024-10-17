639c6003e9602141a6efc6604934a5c411c13df0
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-18 03:06:46 +0900

    fix: FirebaseApp 객체를 주입하지 않은 부분 수정

--- dmforu-fcm/src/main/kotlin/com/dmforu/fcm/FirebaseMessageSender.kt
+++ dmforu-fcm/src/main/kotlin/com/dmforu/fcm/FirebaseMessageSender.kt
@@ -2,6 +2,7 @@ package com.dmforu.fcm
 
 import com.dmforu.admin.message.MessageSender
 import com.dmforu.admin.message.NoticeMessage
+import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 import org.springframework.stereotype.Component
 
@@ -13,8 +14,9 @@ internal class FirebaseMessageSender(
     override fun sendNoticeMessage(message: NoticeMessage, tokens: List<String>) {
 
         val firebaseMessage = firebaseMessageConverter.buildMessageToNotice(message, tokens)
+        val firebaseApp = FirebaseApp.getInstance("DMFORU_APP_PROD")
 
-        FirebaseMessaging.getInstance().sendEachForMulticast(firebaseMessage)
+        FirebaseMessaging.getInstance(firebaseApp).sendEachForMulticast(firebaseMessage)
     }
 
 
