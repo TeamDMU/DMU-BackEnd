1431d1aadd4fbcc6eef709d6492f6f8098f48272
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-17 19:08:04 +0900

    feat: FCM Config 작성

---
+++ dmforu-fcm/src/main/kotlin/com/dmforu/fcm/config/FCMConfig.kt
@@ -0,0 +1,32 @@
+package com.dmforu.fcm.config
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.Resource
+import org.springframework.util.FileCopyUtils
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
+@Configuration
+@EntityScan(basePackages = ["com.dmforu.fcm"])
+class FCMConfig {
+    @Bean
+    @Throws(IOException::class)
+    fun someAppProdFirebaseApp(): FirebaseApp {
+        val credentials = GoogleCredentials.fromStream(ClassPathResource("key/fire-base-key.json").inputStream)
+
+        val options = FirebaseOptions
+            .builder()
+            .setCredentials(credentials)
+            .build()
+
+        return FirebaseApp.initializeApp(options, "DMFORU_APP_PROD")
+    }
+}
