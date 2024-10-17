9bad3da428cc27d0c8652e213e4b26115c8f4d77
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-18 02:31:48 +0900

    chore: spring-context 의존성 제거

--- dmforu-fcm/build.gradle.kts
+++ dmforu-fcm/build.gradle.kts
@@ -2,6 +2,4 @@ dependencies {
     compileOnly(project(":dmforu-domain"))
     compileOnly(project(":dmforu-admin"))
     implementation ("com.google.firebase:firebase-admin:9.2.0")
-
-    compileOnly("org.springframework:spring-context")
 }
