c61439ffa093066b10146f64a29ef98d496d0d40
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-21 21:59:03 +0900

    refactor: 구버전 사용자를 위한 DTO 객체 Deprecated 선언

--- dmforu-api/src/main/kotlin/com/dmforu/api/controller/old/request/OldRegisterSubscribeRequest.kt
+++ dmforu-api/src/main/kotlin/com/dmforu/api/controller/old/request/OldRegisterSubscribeRequest.kt
@@ -2,6 +2,7 @@ package com.dmforu.api.controller.old.request
 
 import com.dmforu.domain.subscribe.Subscribe
 
+@Deprecated("구버전 사용자를 위해 남겨둔 DTO 입니다.")
 data class OldRegisterSubscribeRequest(
     val token: String,
     val department: String,
