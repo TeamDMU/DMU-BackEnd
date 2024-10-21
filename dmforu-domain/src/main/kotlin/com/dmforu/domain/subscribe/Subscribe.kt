4a8ea24f467631d87114e57059a379843e689ba4
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-21 22:40:11 +0900

    refactor: equals, hashcode, toString 메서드 추가

--- dmforu-domain/src/main/kotlin/com/dmforu/domain/subscribe/Subscribe.kt
+++ dmforu-domain/src/main/kotlin/com/dmforu/domain/subscribe/Subscribe.kt
@@ -1,14 +1,12 @@
 package com.dmforu.domain.subscribe
 
 class Subscribe private constructor(
-    token: String,
+    val token: String,
     department: String,
     keywords: List<String>,
     isDepartmentSubscribed: Boolean,
-    isKeywordSubscribed: Boolean
+    isKeywordSubscribed: Boolean,
 ) {
-    val token: String = token
-
     var department: String = department
         private set
 
@@ -61,6 +59,34 @@ class Subscribe private constructor(
 
     fun unsubscribeKeyword() {
         this.isKeywordSubscribed = false
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Subscribe
+
+        if (token != other.token) return false
+        if (department != other.department) return false
+        if (keywords != other.keywords) return false
+        if (isDepartmentSubscribed != other.isDepartmentSubscribed) return false
+        if (isKeywordSubscribed != other.isKeywordSubscribed) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = token.hashCode()
+        result = 31 * result + department.hashCode()
+        result = 31 * result + keywords.hashCode()
+        result = 31 * result + isDepartmentSubscribed.hashCode()
+        result = 31 * result + isKeywordSubscribed.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Subscribe(token='$token', department='$department', keywords=$keywords, isDepartmentSubscribed=$isDepartmentSubscribed, isKeywordSubscribed=$isKeywordSubscribed)"
     }
 
 }
