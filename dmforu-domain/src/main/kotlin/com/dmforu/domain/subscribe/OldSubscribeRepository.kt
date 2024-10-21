42911e0f4395667f8d96c678112ab853fc597d5b
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-21 21:49:55 +0900

    refactor: 구버전 Updater가 Reader와 Writer를 사용하도록 변경 및 비지니스 로직 책임을 엔티티에서 도메인으로 이전

--- dmforu-domain/src/main/kotlin/com/dmforu/domain/subscribe/OldSubscribeRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.dmforu.domain.subscribe
-
-interface OldSubscribeRepository {
-    fun findByIdAndSubscribeDepartment(token: String, department: String)
-    fun findByIdAndUpdateDepartmentUnsubscribe(token: String)
-
-    fun findByIdAndSubscribeKeywords(token: String, keywords: List<String>)
-    fun findByIdAndUpdateKeywordUnsubscribe(token: String)
-}
--- dmforu-infrastructure/storage/db-mysql/src/main/kotlin/com/dmforu/storage/db/mysql/subscribe/SubscribeEntityRepository.kt
+++ dmforu-infrastructure/storage/db-mysql/src/main/kotlin/com/dmforu/storage/db/mysql/subscribe/SubscribeEntityRepository.kt
@@ -1,17 +1,15 @@
 package com.dmforu.storage.db.mysql.subscribe
 
-import com.dmforu.domain.subscribe.OldSubscribeRepository
 import com.dmforu.domain.subscribe.Subscribe
 import com.dmforu.domain.subscribe.SubscribeRepository
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Repository
-import java.util.*
 import kotlin.jvm.optionals.getOrNull
 
 @Repository
 internal class SubscribeEntityRepository(
     private val subscribeJpaRepository: SubscribeJpaRepository,
-) : SubscribeRepository, OldSubscribeRepository {
+) : SubscribeRepository {
 
     @Transactional
     override fun save(subscribe: Subscribe) {
@@ -23,50 +21,12 @@ internal class SubscribeEntityRepository(
         return foundSubscribe.getOrNull()?.toSubscribe()
     }
 
-    @Transactional
-    @Deprecated("구버전 호환성을 위해 남겨둔 메서드입니다.")
-    override fun findByIdAndSubscribeDepartment(token: String, department: String) {
-        val subscribe = findNotNullById(token)
-
-        subscribe.changeDepartment(department)
-
-        subscribe.subscribeDepartment()
-    }
-
-    @Transactional
-    override fun findByIdAndUpdateDepartmentUnsubscribe(token: String) {
-        val subscribe = findNotNullById(token)
-
-        subscribe.unsubscribeDepartment()
-    }
-
-    @Transactional
-    @Deprecated("구버전 호환성을 위해 남겨둔 메서드입니다.")
-    override fun findByIdAndSubscribeKeywords(token: String, keywords: List<String>) {
-        val subscribe = findNotNullById(token)
-
-        subscribe.changeKeywords(keywords)
-
-        subscribe.subscribeKeyword()
-    }
-
-    @Transactional
-    override fun findByIdAndUpdateKeywordUnsubscribe(token: String) {
-        val subscribe = findNotNullById(token)
-
-        subscribe.unsubscribeKeyword()
-    }
-
     override fun findTokensByDepartment(department: String): List<String> {
         return subscribeJpaRepository.findTokensByDepartment(department)
     }
 
     override fun findTokensContainingKeyword(keyword: String): List<String> {
         return subscribeJpaRepository.findTokensContainingKeyword(keyword)
-    }
-
-    private fun findNotNullById(token: String): SubscribeEntity {
-        return subscribeJpaRepository.findById(token).orElseThrow { throw IllegalArgumentException("존재하지 않은 토큰입니다.") }
     }
 
 }
