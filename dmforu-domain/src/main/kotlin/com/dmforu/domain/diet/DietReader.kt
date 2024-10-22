6c428753afc4ce8b7dbbdaadac6758af0cabf3ce
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-22 12:09:40 +0900

    feat: 임시 예외 처리 추후에 변경해야함

--- dmforu-domain/src/main/kotlin/com/dmforu/domain/diet/DietReader.kt
+++ dmforu-domain/src/main/kotlin/com/dmforu/domain/diet/DietReader.kt
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component
 class DietReader(
     private val dietRepository: DietRepository,
 ) {
+    // TODO
     fun read(): List<Diet> {
-        return dietRepository.read().orEmpty()
+        return dietRepository.read() ?: throw IllegalStateException("식단 정보가 존재하지 않습니다. 관리자 에러")
     }
 }
--- dmforu-domain/src/main/kotlin/com/dmforu/domain/schedule/ScheduleReader.kt
+++ dmforu-domain/src/main/kotlin/com/dmforu/domain/schedule/ScheduleReader.kt
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component
 class ScheduleReader(
     private val scheduleRepository: ScheduleRepository
 ) {
+    // TODO
     fun read(): List<Schedule.Year> {
-        return scheduleRepository.read().orEmpty()
+        return scheduleRepository.read() ?: throw IllegalArgumentException("No schedule exists 관리자 에러")
     }
 }
