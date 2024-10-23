c3d87b735620751b75b9593effae13c6188b20a5
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-24 03:32:49 +0900

    chore: 테스트를 위한 mockito 의존성 추가

--- dmforu-domain/build.gradle.kts
+++ dmforu-domain/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+    testImplementation("org.mockito:mockito-junit-jupiter")
     testImplementation("org.mockito:mockito-core")
     testImplementation("org.assertj:assertj-core")
 }
