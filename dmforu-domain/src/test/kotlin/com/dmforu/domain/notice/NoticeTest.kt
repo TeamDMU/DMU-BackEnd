24b55336d10650d00cf15cbaa6ea97bda03c1bb8
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-23 21:43:25 +0900

    test: Notice 인스턴스 생성 메서드 테스트 추가

--- dmforu-domain/src/test/kotlin/com/dmforu/domain/notice/NoticeTest.kt
+++ dmforu-domain/src/test/kotlin/com/dmforu/domain/notice/NoticeTest.kt
@@ -1,11 +1,35 @@
 package com.dmforu.domain.notice
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class NoticeTest {
+
+    @DisplayName("공지를 생성할 수 있다.")
+    @Test
+    fun of() {
+        // given
+        val number = 10
+        val type = "대학"
+        val date = LocalDate.of(2024, 10, 23)
+        val title = "공지사항"
+        val author = "관리자"
+        val url = "https://www.test.com"
+
+        val notice = Notice.of(number, type, date, title, author, url)
+
+        // when // then
+        assertThat(notice.number).isEqualTo(number)
+        assertThat(notice.type).isEqualTo(type)
+        assertThat(notice.date).isEqualTo(date)
+        assertThat(notice.title).isEqualTo(title)
+        assertThat(notice.author).isEqualTo(author)
+        assertThat(notice.url).isEqualTo(url)
+
+    }
 
     @DisplayName("공지의 번호가 특정 숫자보다 작거나 같은지 확인할 수 있다.")
     @Test
