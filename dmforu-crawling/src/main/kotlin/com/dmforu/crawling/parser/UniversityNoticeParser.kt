bfdc6b3e9e7ac6b3887328b9889d1116ad3f20f5
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-25 19:29:23 +0900

    test: University Notice Parser 테스트 작성

--- dmforu-crawling/src/main/kotlin/com/dmforu/crawling/parser/UniversityNoticeParser.kt
+++ dmforu-crawling/src/main/kotlin/com/dmforu/crawling/parser/UniversityNoticeParser.kt
@@ -8,15 +8,10 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class UniversityNoticeParser(
-    private val htmlLoader: HtmlLoader<Document>
+    private val htmlLoader: HtmlLoader<Document>,
 ) : Parser<Notice> {
     private var pageNumber = 1
 
-    /**
-     * HTML을 파싱하여 대학 공지사항 목록을 반환한다.
-     *
-     * @return 대학 공지사항 목록
-     */
     override fun parse(): List<Notice> {
         val universityNotices: MutableList<Notice> = java.util.ArrayList<Notice>()
 
@@ -49,36 +44,22 @@ class UniversityNoticeParser(
 
             universityNotices.add(universityNotice)
         }
-
+        String
         return universityNotices
     }
 
-    /**
-     * 파싱할 페이지의 URL을 생성한다.
-     *
-     * @return URL
-     */
     private fun generateSearchUrl(): String {
-        return String.format(SEARCH_URL, pageNumber++)
+        return StringBuilder()
+            .append("https://www.dongyang.ac.kr/dongyang/129/subview.do?page=").append(pageNumber++)
+            .toString()
     }
 
-    /**
-     * 파싱 결과를 통해 공지사항의 URL를 생성한다.
-     *
-     * @param url URL 파싱결과
-     * @return 공지사항 URL
-     */
     private fun generateUrlFromSearch(url: String): String {
-        return String.format(RESULT_URL, url)
+        return StringBuilder()
+            .append("https://www.dongyang.ac.kr").append(url).append("?layout=unknown")
+            .toString()
     }
 
-    /**
-     * 접미사를 제거한다.
-     *
-     * @param title  원본 문자열
-     * @param suffix 제거하고 싶은 접미사
-     * @return 원본에서 접미사를 제거한 문자열
-     */
     private fun removeSuffix(title: String, suffix: String): String {
         if (title.endsWith(suffix)) {
             return title.substring(0, title.length - suffix.length).trim { it <= ' ' }
@@ -89,8 +70,6 @@ class UniversityNoticeParser(
 
     companion object {
         private val DATE_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-        private const val SEARCH_URL = "https://www.dongyang.ac.kr/dongyang/129/subview.do?page=%d"
-        private const val RESULT_URL = "https://www.dongyang.ac.kr%s?layout=unknown"
         private const val SUFFIX_NEW_POST = "새글"
         private const val NOTICE_TYPE = "대학"
     }
