a5890eff209e26087fe2c09e0299ab34daf12fe1
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-28 18:23:40 +0900

    test: Diet Controller API Test 작성

--- dmforu-api/src/test/kotlin/com/dmforu/api/controller/v1/DietControllerTest.kt
+++ dmforu-api/src/test/kotlin/com/dmforu/api/controller/v1/DietControllerTest.kt
@@ -1,5 +1,38 @@
 package com.dmforu.api.controller.v1
 
+import com.dmforu.api.support.response.ResultType
+import com.dmforu.domain.diet.DietReader
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-class DietControllerTest
+@WebMvcTest(controllers = [DietController::class])
+class DietControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var dietReader: DietReader
+
+    @DisplayName("식단을 불러온다.")
+    @Test
+    fun readDiet() {
+        mockMvc.perform(
+            get("/api/v1/cafeteria")
+                .contentType(APPLICATION_JSON)
+        )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.result").value(ResultType.SUCCESS.name))
+            .andExpect(jsonPath("$.data").isArray)
+            .andExpect(jsonPath("$.error").isEmpty)
+    }
+}
