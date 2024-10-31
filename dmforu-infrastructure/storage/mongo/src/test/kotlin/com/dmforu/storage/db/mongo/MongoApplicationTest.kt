acdd79ae667d0ff4a72f2a85fbf804f982d596c9
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-31 20:58:49 +0900

    fix: mongo module Build 테스트 실패 해결

--- dmforu-infrastructure/storage/mongo/src/test/kotlin/com/dmforu/storage/db/mongo/MongoApplicationTest.kt
+++
@@ -1,6 +0,0 @@
-package com.dmforu.storage.db.mongo
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class MongoApplicationTest
--- dmforu-infrastructure/storage/mongo/src/test/kotlin/com/dmforu/storage/db/mongo/MongoTestSupport.kt
+++ dmforu-infrastructure/storage/mongo/src/test/kotlin/com/dmforu/storage/db/mongo/MongoTestSupport.kt
@@ -1,13 +1,17 @@
 package com.dmforu.storage.db.mongo
 
 import com.dmforu.storage.db.mongo.config.MongoConfig
+import com.dmforu.storage.db.mongo.diet.DietEntityRepositoryTest
+import com.dmforu.storage.db.mongo.schedule.ScheduleEntityMongoRepositoryTest
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 @TestPropertySource(properties = ["spring.config.location = src/main/resources/mongo.yml"])
-@SpringBootTest(classes = [MongoApplicationTest::class])
+@SpringBootTest(classes = [ScheduleEntityMongoRepositoryTest::class, DietEntityRepositoryTest::class])
+@SpringBootApplication
 @Import(MongoConfig::class)
 @ActiveProfiles("test")
 class MongoTestSupport
