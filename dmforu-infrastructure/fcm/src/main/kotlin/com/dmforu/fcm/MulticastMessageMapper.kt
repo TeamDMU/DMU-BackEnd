4c02d4bf4a7704f1c4808dfdaa218122861e963e
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-01 19:55:27 +0900

    refactor: Multicast Message Converter를 Object 클래스로 변경

--- dmforu-infrastructure/fcm/src/main/kotlin/com/dmforu/fcm/MulticastMessageMapper.kt
+++ dmforu-infrastructure/fcm/src/main/kotlin/com/dmforu/fcm/MulticastMessageMapper.kt
@@ -3,12 +3,10 @@ package com.dmforu.fcm
 import com.dmforu.domain.message.NoticeMessage
 import com.google.firebase.messaging.MulticastMessage
 import com.google.firebase.messaging.Notification
-import org.springframework.stereotype.Component
 
-@Component
-internal class FirebaseMessageConverter {
+internal object MulticastMessageMapper {
 
-    fun buildMessageToNotice(message: NoticeMessage, tokens: List<String>): MulticastMessage {
+    fun mapToMulticastMessage(message: NoticeMessage, tokens: List<String>): MulticastMessage {
         val notification = Notification.builder()
             .setTitle(message.title)
             .setBody(message.body)
