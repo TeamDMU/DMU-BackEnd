26d48f47f311d773fc9c2ae5ab9c596b402cc00e
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-02 04:36:34 +0900

    fix: Jacoco 리포트에 예외가 적용되지 않았던 문제 해결

--- dmforu-support/jacoco/build.gradle.kts
+++ dmforu-support/jacoco/build.gradle.kts
@@ -6,3 +6,39 @@ dependencies {
     jacocoAggregation(project(":dmforu-api"))
     jacocoAggregation(project(":dmforu-admin"))
 }
+
+tasks.testCodeCoverageReport {
+    reports {
+        csv.required = true
+        xml.required = false
+    }
+    classDirectories.setFrom(
+        files(
+            listOf(
+                project(":dmforu-api"),
+                project(":dmforu-admin"),
+                project(":dmforu-domain"),
+                project(":dmforu-crawling"),
+
+                project(":dmforu-infrastructure:fcm"),
+                project(":dmforu-infrastructure:storage:mysql"),
+                project(":dmforu-infrastructure:storage:mongo"),
+            ).map {
+                it.fileTree("${it.buildDir}/classes/kotlin/main") {
+                    exclude(
+                        "**/*Application*",
+                        "**/*Config*",
+                        "**/*Dto*",
+                        "**/*Error*",
+                        "**/*Request*",
+                        "**/*Response*",
+                        "**/*Interceptor*",
+                        "**/*Exception*",
+                        "**/*TestSupport*"
+                    )
+                }
+
+            }
+        )
+    )
+}
