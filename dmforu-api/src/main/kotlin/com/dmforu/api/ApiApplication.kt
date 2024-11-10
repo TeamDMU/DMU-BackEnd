8b2cfc2b1718dab86bd741381f6af1e525f323cd
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-11 03:14:46 +0900

    fix: API 서버 Timezone Asia로 설정

--- dmforu-api/src/main/kotlin/com/dmforu/api/ApiApplication.kt
+++ dmforu-api/src/main/kotlin/com/dmforu/api/ApiApplication.kt
@@ -3,6 +3,7 @@ package com.dmforu.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.util.*
 
 @ConfigurationPropertiesScan
 @SpringBootApplication(
@@ -16,5 +17,6 @@ import org.springframework.boot.runApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<ApiApplication>(*args)
 }
